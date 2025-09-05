1cb135a36adc5bcb5c0e8db60800568fd87ff47c
Dart1516 <david_daniel_puche_ferrer@hotmail.com>
2025-09-05 19:35:52 -0300

    fixing cart, I oly add 3 lines of code, but a lot of comments to understand better

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,29 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  
+  // my notes:
+  // I'm using JSON.parse(...)
+//  because this will transfor everythin into a valid JSON array that I can use array methods on
+  // so-carty is the key I'm using to store the cart in local storage
+  //  I retrieve the cart from local storage and parse it into a JavaScript array
+  let cart = JSON.parse(localStorage.getItem("so-cart"));
+  //  if I don't do this and try to use array methods on null it will throw an error
+  //  so I check if it's an array using Array.isArray()
+  //  if it's not an array I set it to an empty array
+  //  then I can safely use array methods on it
+  
+
+  if (!Array.isArray(cart)) cart = [];   // <-- IF this is not an array it will set it to an empty array
+  // add the product to the cart array
+  cart.push(product);
+  //  finally I push the new product to the array and save it back to local storage
+  //  using the setLocalStorage function from utils.mjs
+  //  this function stringifies the array and saves it to local storage
+  //  under the key "so-cart"
+  setLocalStorage("so-cart", product); 
+  //  so now the cart in local storage is updated with the new product
+  //  I can retrieve it later and it will include the new product
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
