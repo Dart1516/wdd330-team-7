7e3dadba44d8232345da2a0377dfce03c88d2d04
Dart1516 <david_daniel_puche_ferrer@hotmail.com>
2025-09-13 21:51:19 -0300

    W02: dynamic product detail page (ProductDetails class, template render, addToCart, URL param)

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,19 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+/* Use those lines to create a new function in the utils.mjs file called getParam(param) that you can use to return a parameter from the URL when requested.
+// Example usage:
+const queryString = window.location.search;
+const product = urlParams.get('product')
+const urlParams = new URLSearchParams(queryString);
+*/
+
+// get a parameter from the query string  by name 
+export function getParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
